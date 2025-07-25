a301e19320ae4da414ad6a0bc305b7334a8f96ce
Артём Краковцев <blooender@gmail.com>
2025-07-25 18:45:47 +0700

    docs: add javadoc for AuthInfo class

--- src/main/java/org/ex9/contractorservice/utils/AuthInfo.java
+++ src/main/java/org/ex9/contractorservice/utils/AuthInfo.java
@@ -5,15 +5,27 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.List;
 
+/**
+ * Утилитарный класс для получения информации об аутентифицированном пользователе.
+ * @author Краковцев Артём
+ */
 public final class AuthInfo {
 
     private AuthInfo() {
     }
 
+    /**
+     * Получает имя авторизированного пользователя.
+     * @return имя пользователя
+     */
     public static String getUsername() {
         return SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
     }
 
+    /**
+     * Получает список ролей авторизованного пользователя
+     * @return список ролей.
+     */
     public static List<String> getRoles() {
         return SecurityContextHolder.getContext().getAuthentication().getAuthorities()
                 .stream().map(GrantedAuthority::getAuthority).toList();
